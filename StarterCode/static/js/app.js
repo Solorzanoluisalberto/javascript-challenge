72d01b26614d8dde861a846049f25d079397625d
Luis Solorzano <Solorzanoluis@hotmail.com>
2021-04-01 12:36:30 -0400

    delete just tbody when click button done

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -30,16 +30,16 @@ Boton_Filter.on("click", function() {
     var DateInput = document.getElementById("datetime").value;
     console.log(DateInput)
     console.log(DateInput.length)
-    if (DateInput.length == 0) {
+    if (DateInput.length == 0) { // date value empty
         console.log("sin filtro la tabla");
-        Print_(tableData, DateInput);
+        Print_(DateInput);
     } else {
-        var sigo = DateExist(DateInput);
-        //    console.log(sigo);
-        switch(sigo) {
+        var go_ = DateExist(DateInput); // valid date?
+        //    console.log(go_);
+        switch(go_) {
         case true:
             console.log(`si es fecha ${DateInput} `);
-            Print_(tableData, DateInput)
+            Print_(DateInput)
             break;
         default:
           // code block 
@@ -49,32 +49,32 @@ Boton_Filter.on("click", function() {
     }
 });
 
-function Print_(tableData1, startDate){    
-    var resultData = tableData1.filter(a => {
-    var date = new Date(a.datetime).toLocaleDateString("en-US");
-        // console.log(date);
-        // console.log(new Date(startDate).toLocaleDateString("en-US"));
-        if (startDate.length != 0) {
-            return (date == new Date(startDate).toLocaleDateString("en-US"));
-        } else {
-            return (date == date);
-        }
-    });
-    console.log(resultData);
+function Print_(UFO_Date){    
+    console.log(UFO_Date);
+    var UFO_sighting = tableData.filter(Filter_UFO_sighting, fechas= UFO_Date);
+    console.log(UFO_sighting);
     
-    let empTab = document.getElementById('ufo-table');
+    let UFOTab = document.getElementById('ufo-table');
     let tbody_ = document.getElementById("ufo-table").tBodies[0]
-    tbody_.lastChild.remove();
+    // 
+    if (tbody_ != null) {
+        console.log("exist <tr> in <tbody>");
+        UFOTab.removeChild(tbody_);
+		tbody_ = document.createElement('tbody')
+        UFOTab.append(tbody_)
+        // tbody_.Child.remove();
+        // document.getElementById("ufo-table").tBodies[0].remove();
+    }
     // $("#Your_Table tr>td").remove();
     console.log(tbody_);
     // var rowCnt = tbody_.rows.length;    // get the number of rows.
     // table row.
     // tr = empTab.insertRow(rowCnt);
-    resultData.forEach((view) => {
+    UFO_sighting.forEach((sighting) => {
         let tr = tbody_.insertRow(); 
         // Iterate through each key and value
         var td = document.createElement('td');  // TABLE BODY.
-        Object.entries(view).forEach(([key, value]) => {
+        Object.entries(sighting).forEach(([key, value]) => {
             td = tr.insertCell();
             let text = document.createTextNode(value);
             td.appendChild(text);
@@ -84,29 +84,32 @@ function Print_(tableData1, startDate){
     });
 }
 
-var empTab = document.getElementById('ufo-table');
-var tbody_ = document.getElementById("ufo-table").tBodies[0];
+// var empTab = document.getElementById('ufo-table');
+// var tbody_ = document.getElementById("ufo-table").tBodies[0];
 
-tableData.forEach((view) => {
-    // Iterate through each key and value
-    var tr = tbody_.insertRow();
-    var td = document.createElement('td');  // TABLE BODY.
-    Object.entries(view).forEach(([key, value]) => {
-        td = tr.insertCell();
-        let text = document.createTextNode(value);
-        td.appendChild(text);
-        // console.log(value);
-      // Use the key to determine which array to push the value to
-    });
+// tableData.forEach((view) => {
+//     // Iterate through each key and value
+//     var tr = tbody_.insertRow();
+//     var td = document.createElement('td');  // TABLE BODY.
+//     Object.entries(view).forEach(([key, value]) => {
+//         td = tr.insertCell();
+//         let text = document.createTextNode(value);
+//         td.appendChild(text);
+//         // console.log(value);
+//       // Use the key to determine which array to push the value to
+//     });
 
-});
+// });
 
 // =======================================
-
-
-
-
-
-
-
-
+Filter_UFO_sighting(fechas = '');
+function Filter_UFO_sighting(sighting) {
+    var date = new Date(sighting.datetime).toLocaleDateString("en-US");
+    if (fechas.length==0) { //If the date value is blank, I present all the data in the dataset.
+        return (date == date);
+    } else {
+        var date1 = new Date(fechas).toLocaleDateString("en-US"); // comparitiondate
+        return (date == date1);
+    } 
+  }
+  
